e3fddc30f66cde13df0f3e73acce03a00bf3eff6
Isaiah Wright <isaiahwright@isaiahs-mini.lan>
2022-12-03 21:55:49 -0600

    adding Docker 🐋 & Docker Compose 🐙 for HA & Jellyfin servers

--- install/install.sh
+++ install/install.sh
@@ -7,9 +7,12 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     # make and change to the install directory
     mkdir ~/homekit-server/install
     cd ~/homekit-server/install
+
+
     # Kiwix
     wget https://download.kiwix.org/release/kiwix-desktop/kiwix-desktop_x86_64.appimage # Install Kiwix (https://download.kiwix.org/release/kiwix-desktop/kiwix-desktop_x86_64.appimage)
     chmod +x kiwix-desktop_x86_64.appimage # Make Kiwix executable
+
     # ./kiwix-desktop_x86_64.appimage # Run Kiwix
     # create a systemctl service for Kiwix
     sudo touch /etc/systemd/system/kiwix.service
@@ -26,11 +29,44 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     [Install]
     WantedBy=multi-user.target" > /etc/systemd/system/kiwix.service
     sudo systemctl enable kiwix.service # Enable Kiwix service
-
-    # Install Home Assistant with Git (https://github.com/home-assistant/core.git)
-    git clone https://github.com/home-assistant/core.git
-    cd core
+    sudo systemctl start kiwix.service # Start Kiwix service
 
 
-    # Install Jellyfin
-    sudo apt-get install jellyfin
+    # Install Home Assistant (https://www.home-assistant.io/installation/)
+    # Need to install Docker and Docker Compose first
+    # Install Docker
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    # Add the user to the docker group
+    sudo usermod -aG docker $USER
+
+    # Install Docker Compose
+    sudo apt-get install -y libffi-dev libssl-dev
+    sudo apt-get install -y python3 python3-pip
+    sudo apt-get remove python-configparser
+    sudo pip3 -v install docker-compose
+
+    # Install Home Assistant
+    sudo mkdir /opt/homeassistant
+    sudo chown $USER:$USER /opt/homeassistant
+    cd /opt/homeassistant
+    sudo docker-compose run --rm homeassistant
+    sudo docker-compose up -d
+    sudo docker-compose logs -f
+
+
+    # Install Jellyfin (https://jellyfin.org/docs/general/administration/installing)
+    # TODO: create bind-mount instructions for config and cache
+    docker pull jellyfin/jellyfin
+    docker volume create jellyfin-config
+    docker volume create jellyfin-cache
+
+    docker run -d \
+    --name jellyfin \
+    --user uid:gid \
+    --net=host \
+    --volume jellyfin-config:/config \ # Alternatively --volume /path/to/config:/config 
+    --volume jellyfin-cache:/cache \ # Alternatively  --volume /path/to/cache:/cache
+    --mount type=bind,source=/path/to/media,target=/media \
+    --restart=unless-stopped \
+    jellyfin/jellyfin
