bafbbfae64df2b397055c936441e06d4306c40d0
Isaiah Wright <isaiah.wright@tutanota.com>
2023-06-11 23:38:15 -0500

    rebasing for tx/jackett/sonarr/radarr/jellyfin/blackcandy stack

---
+++ server/start_homekit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIRECTORY=$PWD # TODO: setup cases for mac and windows shell commands
+
+for subdirectory in "$DIRECTORY"/*/; do
+  if [[ "$subdirectory" != "$DIRECTORY/media-server/" ]]; then
+    cd "$subdirectory"
+    docker-compose up -d
+  fi
+done
+
